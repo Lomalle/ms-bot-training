4a53bfac0be47bf6bad015aeb539f87f1475260c
O'Malley, Laura <laura.omalley@accenture.com>
2016-12-20 14:30:08 +0000

    change

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 var _config = {
     CHAT_CONNECTOR: {
-        APP_ID: '0aa9cb00-d909-4605-860d-f00a53c78ce3' || "", //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
-        APP_PASSWORD: 'Uheiio2YGn7aotxKPSPZMNn' || ""
+        APP_ID: process.env.MICROSOFT_APP_ID || "", //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
+        APP_PASSWORD: process.env.MICROSOFT_APP_PASSWORD || ""
     },
     COMPUTER_VISION_SERVICE: {
         API_URL: "https://api.projectoxford.ai/vision/v1.0/",
