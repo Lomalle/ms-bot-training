a1d1f551a8124611e576170805734a0dbc5591c2
O'Malley, Laura <laura.omalley@accenture.com>
2016-12-20 13:55:32 +0000

    Committing first files

---
+++ Node/bot-cognitive-sample/configuration.js
@@ -0,0 +1,11 @@
+var _config = {
+    CHAT_CONNECTOR: {
+        APP_ID: '0aa9cb00-d909-4605-860d-f00a53c78ce3' || "", //You can obtain an APP ID and PASSWORD here: https://dev.botframework.com/bots/new
+        APP_PASSWORD: 'Uheiio2YGn7aotxKPSPZMNn' || ""
+    },
+    COMPUTER_VISION_SERVICE: {
+        API_URL: "https://api.projectoxford.ai/vision/v1.0/",
+        API_KEY: "c9001ad1c0354d4885ff822b4cbf4ac7"  //You can obtain an COGNITIVE SERVICE API KEY: https://www.microsoft.com/cognitive-services/en-us/pricing
+    }
+};
+exports.CONFIGURATIONS = _config;
